b0ae1cc13a1ec2739d9c398043e2f3d92173fd40
Shakti Jain <shaktijain9@gmail.com>
2023-02-26 00:37:13 +0530

    Added folder structure.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sensor',
+    version="0.0.1",
+    author='Shakti Jain',
+    author_email="shaktijain9806@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
