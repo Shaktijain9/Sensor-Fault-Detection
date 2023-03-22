f2abb1d4bfd37c278dccc16e85fae291ff07e0a2
Shakti Jain <shaktijain9@gmail.com>
2023-03-23 00:19:44 +0530

    Added changes for batch prediction.

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from sensor.pipeline.batch_prediction import start_batch_prediction
 if __name__ == "__main__":
     try:
         # start_training_pipeline()
-        start_batch_prediction(
+        output_file_path = start_batch_prediction(
             input_file_path='/Users/shaktijain/PycharmProjects/Sensor-Fault-Detection/aps_failure_training_set1.csv')
+        print(output_file_path)
     except Exception as e:
         raise e
